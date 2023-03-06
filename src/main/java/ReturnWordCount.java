abfe2d1fa69d51e49ca77470c687cb521249168a
Colby Canselo <colbycanselo@gmail.com>
2023-03-06 17:40:48 +0000

    WORD COUNT

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -4,7 +4,16 @@ public class ReturnWordCount {
      * @param in A String representing a sentence, with words delineated by spaces.
      * @return return the amount of words in a string.
      */
-    public int count(String in){
-        return 0;
+
+    public int count(String in) {
+
+        String[] split = in.split(" ");
+        int count = 0;
+
+        for (String word: split) {
+            count += 1;
+        }
+
+        return count;
     }
 }
